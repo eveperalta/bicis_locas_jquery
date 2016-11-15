717d82ba26b42c860c17050a62134b76f0d3302d
EvePeralta <eperalta.cm@gmail.com>
2016-11-15 18:49:31 -0300

    intentando con jquery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,44 @@
-function validateForm(){
-	/* Escribe tú código aquí */
-}
+/*
 #name
 #lastname
 #input-email
 #input-password
 #input-social
+#bEnviar
+*/
+
+
+$(document).ready(function validateForm(){
+var exprEmail = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+
+	$("#bEnviar").click(function(){
+		var nombre = $("#name").val();
+		var apellido = $("#lastname").val();
+		var correo = $("#input-email").val();
+		var pwd = $("#input-password").val();
+
+			if(nombre == ""){
+				$("#mensaje1").fadeIn();
+				return false;
+			}else{
+				$("#mensaje1").fadeOut();
+				if(correo == "" || !exprEmail.test(correo)){
+					$("#mensaje2").fadeIn();
+					return false;
+				}else{
+					$("#mensaje2").fadeOut();
+					if(apellido == ""){
+						$("#mensaje3").fadeIn();
+						return false;
+					}else{
+						$("#mensaje3").fadeout();
+						if(pwd == ""){
+							$("#mensaje4").fadeIn();
+							return false;
+						}
+					}
+				}
+			}
+	});
+
+});
